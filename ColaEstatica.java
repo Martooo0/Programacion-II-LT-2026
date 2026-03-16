37465bb6bbcd2ae90f2d2ea17e640a718388ec97
phinnsolo <139174245+phinnsolo@users.noreply.github.com>
2026-03-16 14:59:39 -0300

    Update ColaEstatica.java

--- ColaEstatica.java
+++ ColaEstatica.java
@@ -13,10 +13,14 @@ public class ColaEstatica implements ColaTDA {
         //
     };
     public int primero(){
-        //
+        // Agarra en la cola el valor que tenga en la posicion 0. Y  lo muestra
     };
     public boolean colaVacia(){
-        //
+        """
+            agarra la cola y se fija si está vacía. Una forma de apliciar es usando el metodo de desacolar y si no saca nada significa es si está 
+            vacía por lo tanto es verdadero, sino es falso.
+
+        """
     };
 
 }
